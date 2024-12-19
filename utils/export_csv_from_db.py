095fdea77a4710633bd871383b31016c74f2672a
R-Grigala <r.grigalashvili777@gmail.com>
2024-12-19 15:32:41 +0400

    add new data

--- utils/export_csv_from_db.py
+++ utils/export_csv_from_db.py
@@ -1,24 +1,38 @@
 import pymysql
 import csv
+import os
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path='../.env')
+
+MYSQL_HOST = os.getenv('MYSQL_HOST', 'default_host')
+MYSQL_DATABASE = os.getenv('MYSQL_DATABASE', 'default_database')
+MYSQL_USER = os.getenv('MYSQL_USER', 'default_user')
+MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD_CSV', 'default_password')
+
+start_date = '2024-06-26'
+end_date = '2024-06-28'
 
 # Database connection
 connection = pymysql.connect(
-    host="localhost",
-    user="root",
-    password="Ml@Root88",
-    database="weather"
+    host=MYSQL_HOST,
+    user=MYSQL_USER,
+    password=MYSQL_PASSWORD,
+    database=MYSQL_DATABASE
 )
 
 cursor = connection.cursor()
 
 # SQL query
-query = "SELECT * FROM weather_data WHERE precip_time > '2024-06-26' AND precip_time < '2024-06-28'"
+query = f"SELECT * FROM weather_data WHERE precip_time > '{start_date}' AND precip_time < '{end_date}'"
+
 cursor.execute(query)
 
 # Fetch data
 rows = cursor.fetchall()
+
 # Write to CSV
-output_file = '../weather_data_2024-06-26_2024-06-28.csv'
+output_file = f'../weather_data_{start_date}_{end_date}.csv'
 
 with open(output_file, mode='w', newline='', encoding='utf-8') as file:
     writer = csv.writer(file)
