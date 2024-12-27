d6270a38b450af89651ef2976c1dbd664c52810b
varsimashviliLuka <varsimashvili.official@gmail.com>
2024-12-27 14:20:46 +0400

    database connection with flask in update_temporary_db

--- utils/update_temporary_db/update_temporary_db.py
+++ utils/update_temporary_db/update_temporary_db.py
@@ -4,7 +4,7 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
 
-from src.models import Stations, WeatherData, StationsDivPositions
+from src.models import StationsDivPositions
 from src import create_app
 
 
