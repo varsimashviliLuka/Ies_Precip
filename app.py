cf6786dee54406f8af04d19fa85b7b86709179be
varsimashviliLuka <varsimashvili.official@gmail.com>
2025-02-07 10:41:06 +0400

    app

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from src import create_app
-from src.config import TestConfig
+# from src.config import TestConfig
 
-flask_app = create_app(TestConfig)
+# flask_app = create_app(TestConfig)
 
-# flask_app = create_app()
+flask_app = create_app()
 
 if __name__ == "__main__":
     flask_app.run(debug=True, host='0.0.0.0')
