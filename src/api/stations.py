a66d3c82cd633625d04a0f8a36b79f6da44ba815
varsimashviliLuka <varsimashvili.official@gmail.com>
2025-02-19 11:49:51 +0400

    optimized link checkers while editing stations

--- src/api/stations.py
+++ src/api/stations.py
@@ -127,13 +127,13 @@ class StationsAPI(Resource):
         
         args = stations_parser.parse_args()
 
+        checker_station = Stations.query.filter(Stations.url == args.get('url'), Stations.id != id).first()
+
+        if checker_station:
+            return {'error': 'აღნიშნული სადგური უკვე არსებობს!'}, 400
+
         shorten_station_name = args.get('url').split('/')[-1]
         api_url = f'https://api.weather.com/v2/pws/observations/current?apiKey=e1f10a1e78da46f5b10a1e78da96f525&stationId={shorten_station_name}&numericPrecision=decimal&format=json&units=m'
-
-        response = requests.get(api_url)
-
-        if response.status_code != 200:
-            return {'error': 'გთხოვთ შეიყვანეთ სწორი ლინკი'}, 400
 
 
         station.station_name = args.get('station_name')
