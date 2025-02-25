13fefec3dc4ca456630ca1dbcfcdb4336e2af6cb
R-Grigala <r.grigalashvili777@gmail.com>
2025-02-25 17:51:31 +0400

    working send mail

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -4,3 +4,4 @@ from src.api.filter import FilterAPI
 from src.api.authentication import RegistrationApi, AuthorizationApi, AccessTokenRefreshApi
 from src.api.div_positions import DivPositions
 from src.api.export import Export_API
+from src.api.send_mail import SendEmail
